ccdb2d5920bdf23b0f53e24a55dc19360bc3c977
Pedro Almeida <pedro.gregorio96@gmail.com>
2022-04-15 13:28:57 +0100

    doctor creation improvement

--- management/src/main/java/com/pedroalmeida/doctor/service/DoctorService.java
+++ management/src/main/java/com/pedroalmeida/doctor/service/DoctorService.java
@@ -1,5 +1,6 @@
 package com.pedroalmeida.doctor.service;
 
+import com.pedroalmeida.appointment.model.entity.Appointment;
 import com.pedroalmeida.doctor.model.entity.Doctor;
 
 import java.util.List;
@@ -7,4 +8,13 @@ import java.util.Optional;
 
 public interface DoctorService {
     List<Doctor> getAllDoctors();
+
+    Doctor updateDoctor(Doctor doctor);
+
+    void deleteDoctor(Doctor doctor);
+
+    Doctor insertDoctor(Doctor doctor);
+
+    Optional<Doctor> getById(Long id);
+
 }
--- management/src/main/java/com/pedroalmeida/shared/Mappings.java
+++ management/src/main/java/com/pedroalmeida/shared/Mappings.java
@@ -10,10 +10,10 @@ public class Mappings {
     public static final String UPDATE_APPOINTMENT = "/patch";
 
     public static final String GET_ALL_DOCTORS = "/doctors";
-    public static final String GET_DOCTORS_BY_ID = "/doctors/{id}";
-    public static final String ADD_DOCTORS = "/doctors/add";
-    public static final String DELETE_DOCTORS = "/doctors/delete/{id}";
-    public static final String UPDATE_DOCTORS = "/doctors/patch";
+    public static final String GET_DOCTOR_BY_ID = "/doctors/{id}";
+    public static final String ADD_DOCTOR = "/doctors/add";
+    public static final String DELETE_DOCTOR = "/doctors/delete/{id}";
+    public static final String UPDATE_DOCTOR = "/doctors/patch";
 
     public static final String GET_ALL_PATIENTS = "/patients";
     public static final String GET_PATIENTS_BY_ID = "/patients/{id}";
