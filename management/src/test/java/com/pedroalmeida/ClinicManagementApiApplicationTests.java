20461709b07d8023cdb01baaa1cb88ada6aaabf6
pedroalmeida96 <pedro.gregorio96@gmail.com>
2022-05-14 18:46:06 +0100

    correction ClinicManagementApiApplicationTests

---
+++ management/src/test/java/com/pedroalmeida/ClinicManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroalmeida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
