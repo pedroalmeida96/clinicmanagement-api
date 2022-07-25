bb4af08fe739a3e8ad8aed926ad0d1821779c647
pedroalmeida96 <pedro.gregorio96@gmail.com>
2022-07-25 22:13:02 +0100

    patient fully implemented

--- management/src/main/java/com/pedroalmeida/patient/service/PatientService.java
+++ management/src/main/java/com/pedroalmeida/patient/service/PatientService.java
@@ -3,7 +3,16 @@ package com.pedroalmeida.patient.service;
 import com.pedroalmeida.patient.model.entity.Patient;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PatientService {
     List<Patient> getAllPatients();
+
+    Patient updatePatient(Patient patient);
+
+    void deletePatient(Patient patient);
+
+    Patient insertPatient(Patient patient);
+
+    Optional<Patient> getById(Long id);
 }
